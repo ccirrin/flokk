49303e38774bc086a6fb573bd18c47a30ddb96f6
Carlo Cirrincione <ccirrin@gmail.com>
2017-10-13 13:45:02 -0400

    Implemented a button from HomeActivity to MyEventsActivity

--- app/src/main/java/org/launchpadcs/flokk/HomeActivity.java
+++ app/src/main/java/org/launchpadcs/flokk/HomeActivity.java
@@ -1,13 +1,24 @@
 package org.launchpadcs.flokk;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
-
+    private Button myEventsButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        myEventsButton = (Button) findViewById(R.id.myEventsButton);
+        myEventsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, MyEventsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
